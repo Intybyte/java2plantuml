9247eea80e9ffe6b5f8293a25ccce846d2e6ec5e
Intybyte <vaan.testwork@gmail.com>
2025-06-12 12:40:53 +0200

    Maybe in the future separate all the write methods

---
+++ src/main/java/translate/component/SetTranslatingComponent.java
@@ -0,0 +1,42 @@
+package translate.component;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class SetTranslatingComponent<T> implements TranslatingComponent {
+    protected Set<T> set = new HashSet<>();
+    protected Class<T> type;
+
+    protected SetTranslatingComponent(Class<T> type) {
+        this.type = type;
+    }
+
+    public void safeAdd(Object object) {
+        if (type.isInstance(object)) {
+            set.add(type.cast(object));
+        }
+    }
+
+    public void add(T... elements) {
+        set.addAll(Arrays.asList(elements));
+    }
+
+    public void add(Collection<? extends T> collection) {
+        set.addAll(collection);
+    }
+
+    public Class<T> type() {
+        return type;
+    }
+
+    @Override
+    public void write(StringBuilder builder) {
+        for (var element : set) {
+            writeComponent(element, builder);
+        }
+    }
+
+    abstract void writeComponent(T element, StringBuilder builder);
+}
---
+++ src/main/java/translate/component/TranslatingComponent.java
@@ -0,0 +1,5 @@
+package translate.component;
+
+public interface TranslatingComponent {
+    void write(StringBuilder builder);
+}
